3e8c2804b91922bea5f76a9b75854fc50a56fb2c
Jovin <s10243666@connect.np.edu.sg>
2023-05-01 20:11:40 +0800

    new commit, oh nos, why not wokring

--- app/src/main/java/sg/edu/np/mad/mad_w2_practical/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/mad_w2_practical/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.example.week2practical.R;
+import sg.edu.np.mad.madpractical.R;
 
 
 public class MainActivity extends AppCompatActivity {
