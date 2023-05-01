7f4ff8e9a186a71de8e4380c226b7a17e441d050
Jovin <s10243666@connect.np.edu.sg>
2023-05-01 19:28:31 +0800

    new commit

--- app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
@@ -3,12 +3,46 @@ package sg.edu.np.mad.madpractical;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    final String Title = "Main Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        User myUser = new User();
+
+        TextView header = findViewById(R.id.textView);
+        header.setText("     MAD Practical");
+
+        TextView userName = findViewById(R.id.textView2);
+        userName.setText("Hello World!");
+
+        TextView userDescription = findViewById(R.id.textView3);
+        userDescription.setText("The beauty of programming lies in the fact that it constantly challenges you to come up with innovative solutions to complex problems. Programming allows for continuous learning and improvement, where one can constantly discover new technologies and stay up to date with the latest trends.");
+
+        Button myButton = findViewById(R.id.button);
+        myUser.setFollowed(false);
+        myButton.setOnClickListener(new View.OnClickListener() { //inner class
+            @Override
+            public void onClick(View v) { //effecting what it would do after u click on the button
+                Log.v(Title, "Follow button is pressed");
+                if (myUser.isFollowed() == false){
+                    myButton.setText("FOLLOW");
+                    myUser.setFollowed(true);
+                }
+                else {
+                    myButton.setText("UNFOLLOW");
+                    myUser.setFollowed(false);
+                }
+            }
+        });
+
+
     }
 }
